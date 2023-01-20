1faa64299ebe32e8df4a8d54c7d502390b527120
Josias Navasca <76810853+T3chnopath@users.noreply.github.com>
2023-01-20 04:50:41 -0800

    Implemented draft decreasing function

--- src/LandedVerification.py
+++ src/LandedVerification.py
@@ -32,6 +32,22 @@ def average(sensor, value, samples):
         total = list(filter((None, None, None).__ne__, total))
         return np.average(total, axis = 0)
 
+def decreasing(sensor, value, samples):
+    x = 0
+    while(x < 1000):
+        
+        sample1 = []
+        sample2 = []
+
+        sample1 = average(sensor, value, samples)
+        sample2 = average(sensor, value, samples)
+
+        difference = np.subtract(sample1, sample2)
+        
+        if(difference < 0):
+            x += 1
+
+
 
 while True:
     start = time.time()
