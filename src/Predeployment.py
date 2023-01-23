4487edbb50a64e3265ba88ed7829cf05f67e465f
t3chnopath <josias.navasca@gmail.com>
2023-01-23 01:37:16 -0800

    Removed debug printout function

--- src/Predeployment.py
+++ src/Predeployment.py
@@ -24,17 +24,6 @@ imu = adafruit_bno055.BNO055_I2C(i2c)
 alt = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
 
-def printout():
-    print("Accelerometer (m/s^2): {}".format(imu.acceleration))
-    print("Magnetometer (microteslas): {}".format(imu.magnetic))
-    print("Gyroscope (rad/sec): {}".format(imu.gyro))
-    print("Euler angle: {}".format(imu.euler))
-    print("Quaternion: {}".format(imu.quaternion))
-    print("Linear acceleration (m/s^2): {}".format(imu.linear_acceleration))
-    print("Gravity (m/s^2): {}".format(imu.gravity))
-    print("Pressure: {:6.4f}  Temperature: {:5.2f}".format(alt.pressure, alt.temperature))
-
-
 def average(sensor, value, samples):
     total = []
     for x in range(samples):
