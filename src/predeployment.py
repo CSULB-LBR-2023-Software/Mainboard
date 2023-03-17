1a6dba3ff1b718d225b973c66f8d0e97d88e71ef
T3chnopath <josias.navasca@gmail.com>
2023-03-16 19:40:36 -0700

    Added initial sensor test

---
+++ src/predeployment.py
@@ -0,0 +1,50 @@
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+from adafruit_extended_bus import ExtendedI2C as I2C
+import adafruit_bno055
+import adafruit_bmp3xx
+
+i2c = I2C(5)  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+# If you are going to use UART uncomment these lines
+# uart = board.UART()
+# sensor = adafruit_bno055.BNO055_UART(uart)
+
+last_val = 0xFFFF
+
+
+def temperature():
+    global last_val  # pylint: disable=global-statement
+    result = sensor.temperature
+    if abs(result - last_val) == 128:
+        result = sensor.temperature
+        if abs(result - last_val) == 128:
+            return 0b00111111 & result
+    last_val = result
+    return result
+
+
+while True:
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    """
+    print(
+        "Temperature: {} degrees C".format(temperature())
+    )  # Uncomment if using a Raspberry Pi
+    """
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print(
+        "Altitude: {:6.4f} Pressure: {:6.4f}  Temperature: {:5.2f}".format(bmp.altitude, bmp.pressure, bmp.temperature)
+    )
+    print()
+
+    time.sleep(1)
+
